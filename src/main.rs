9baf2936fa9b1ebc5ef982988d49bf2a31e0b7d4
tingfeng-key <1770963469@qq.com>
2019-09-11 14:05:40 +0800

    更新支持更换音频源

--- src/main.rs
+++ src/main.rs
@@ -1,97 +1,162 @@
-extern crate cpal;
-use cpal::{StreamData, UnknownTypeOutputBuffer};
-use cpal::traits::{DeviceTrait, HostTrait, EventLoopTrait};
-use minimp3::Decoder;
-use std::fs::File;
-use minimp3::Frame;
-use minimp3::Error;
-use std::path::Path;
-use std::sync::mpsc::channel;
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
 
 fn main() {
+    let mut audio = AudioPlayBack::new();
+    audio.playback();
+    audio.run("res/dnxk.mp3");
+    thread::sleep(Duration::from_secs(10));
+    audio.run("res/情歌王 - 古巨基.mp3");
+    thread::park();
 
-    let mut decoder = Decoder::new(File::open(
-        Path::new("res/dnxk.mp3")
-    ).unwrap());//1559457768
+}
+//控制器
+struct AudioControl {
+    speed: usize
+}
+//音频数据结构
+struct Audio {
+    control: AudioControl,
+    source_path: Option<String>,
+    current_source_data: Vec<i16>,
+    current_frame_channel: usize,
+    current_frame_data_index: usize
+}
 
-    let host = cpal::default_host();
-    let device = host.default_output_device().expect("Failed to get default output device");
-    let format = device.default_output_format().expect("Failed to get default output format");
-    let event_loop = host.event_loop();
-    let stream_id = event_loop.build_output_stream(&device, &format).unwrap();
-    event_loop.play_stream(stream_id.clone());
+impl Audio {
+    //实例
+    pub fn new() -> Self {
+        let control = AudioControl {
+            speed: 1
+        };
+        Self {
+            control,
+            source_path: None,
+            current_frame_channel: 0,
+            current_source_data: Vec::new(),
+            current_frame_data_index: 0
+        }
+    }
+    //播放
+    pub fn play(&mut self, source_path: &str) {
+        self.source_path = Some(String::from(source_path));
+        self.current_source_data = Vec::new();
+        self.current_frame_data_index = 0;
+        self.decoder();
+    }
+    //解码
+    fn decoder(&mut self) {
+        use minimp3::Decoder;
+        use std::{fs::File, path::Path};
 
-    let mut data = Vec::new();
-    let mut frame_channel = 0;
-    let speed = 1;
-    loop {
-        match decoder.next_frame() {
-            Ok(mut f) => {
-                /*song_channels = f.channels;
-                song_sample_rate = f.sample_rate;*/
-                for sample in f.data.chunks_mut(f.channels as usize) {
-                    for a in sample.iter_mut() {
-                        data.push(a.clone());
+        let source_path = self.source_path.clone().unwrap();
+        let source_file = File::open(
+            Path::new(&source_path)
+        ).expect("打开资源文件失败...");
+        let mut decoder = Decoder::new(source_file);
+        loop {
+            match decoder.next_frame() {
+                Ok(mut f) => {
+                    for sample in f.data.chunks_mut(f.channels as usize) {
+                        for a in sample.iter_mut() {
+                            self.current_source_data.push(a.clone());
+                        }
+                    }
+                    if self.current_frame_channel == 0 {
+                        self.current_frame_channel = f.channels
                     }
                 }
-                if frame_channel == 0 {
-                    frame_channel = f.channels
+                Err(_) => {
+                    println!("解码完成...");
+                    break;
                 }
-            }
-            Err(_) => {
-                break;
             }
         }
     }
-
-    let mut current_frame_data_index = 0;
-    let mut next_value = || {
-        let s = match current_frame_data_index < data.len() {
+    //获取输出值
+    fn get_next_value(&mut self) -> f32 {
+        let s = match self.current_frame_data_index < self.current_source_data.len() {
             true => {
-                current_frame_data_index += (frame_channel/frame_channel * speed) as usize;
-                data[current_frame_data_index]
+                let speed = self.control.speed.clone();
+                let channel = self.current_frame_channel.clone();
+                let value = self.current_source_data[self.current_frame_data_index];
+                let inc_index = channel / channel * speed;
+                self.current_frame_data_index += inc_index;
+                value
             },
             false => {
                 0i16
             }
         };
         s as f32 / std::i16::MAX as f32// as f32 / std::i16::MAX as f32
-    };
-    event_loop.run(move |stream_id, stream_result| {
-        let stream_data = match stream_result {
-             Ok(data) => data,
-             Err(err) => {
-                 eprintln!("an error occurred on stream {:?}: {}", stream_id, err);
-                 return;
-             }
-             _ => {
-                 println!("empty data");
-                 return;
-             },
-         };
-        match stream_data {
-            cpal::StreamData::Output { buffer: cpal::UnknownTypeOutputBuffer::U16(mut buffer) } => {
-                println!("1");
-                for sample in buffer.chunks_mut(format.channels as usize) {
-                    for out in sample.iter_mut() {
-                        *out = 0;
-                    }
-                }
-            },
-            cpal::StreamData::Output { buffer: cpal::UnknownTypeOutputBuffer::I16(mut buffer) } => {
-                println!("2");
-                for elem in buffer.iter_mut() {
-                    *elem = 0;
-                }
-            },
-            cpal::StreamData::Output { buffer: cpal::UnknownTypeOutputBuffer::F32(mut buffer) } => {
-                for sample in buffer.chunks_mut(format.channels as usize) {
-                    for out in sample.iter_mut() {
-                        *out = next_value();
-                    }
-                }
-            },
-            _ => (),
+    }
+}
+
+//后台播放器
+struct AudioPlayBack {
+    audio: Arc<Mutex<Audio>>,
+}
+
+impl AudioPlayBack {
+    pub fn new() -> Self {
+        Self {
+            audio: Arc::new(Mutex::new(Audio::new()))
         }
-    });
+    }
+    //播放音频
+    pub fn run(&self, source_path: &str) {
+        self.audio.lock().unwrap().play(source_path);
+    }
+    //后台运行
+    pub fn playback(&mut self) {
+        extern crate cpal;
+        use std::thread;
+        use cpal::{
+            StreamData::Output,
+            UnknownTypeOutputBuffer::{U16, I16, F32},
+            traits::{DeviceTrait, HostTrait, EventLoopTrait}
+        };
+
+        let host = cpal::default_host();
+        let device = host.default_output_device().expect("Failed to get default output device");
+        let format = device.default_output_format().expect("Failed to get default output format");
+        let event_loop = host.event_loop();
+        let stream_id = event_loop.build_output_stream(&device, &format).unwrap();
+        event_loop.play_stream(stream_id.clone()).unwrap();
+        let audio = self.audio.clone();
+        thread::spawn(move || {
+            event_loop.run(move |stream_id, stream_result| {
+                let stream_data = match stream_result {
+                    Ok(data) => data,
+                    Err(err) => {
+                        eprintln!("an error occurred on stream {:?}: {}", stream_id, err);
+                        return;
+                    }
+                };
+                match stream_data {
+                    Output { buffer: U16(mut buffer) } => {
+                        for sample in buffer.chunks_mut(format.channels as usize) {
+                            for out in sample.iter_mut() {
+                                *out = 0;
+                            }
+                        }
+                    },
+                    Output { buffer: I16(mut buffer) } => {
+                        for elem in buffer.iter_mut() {
+                            *elem = 0;
+                        }
+                    },
+                    Output { buffer: F32(mut buffer) } => {
+                        for sample in buffer.chunks_mut(format.channels as usize) {
+                            for out in sample.iter_mut() {
+                                *out = audio.lock().unwrap().get_next_value();
+                            }
+                        }
+                    },
+                    _ => (),
+                }
+            });
+        });
+    }
 }
